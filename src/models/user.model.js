fe59218e6bcb373a928c2bc61cae61b3f13dbce4
Rajkumar <kuraj10101999@gmail.com>
2023-12-20 00:46:29 +0530

    done modal

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -50,4 +52,39 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+});
+/* We are injecting the methods using pipline =>mongoose-aggregate-paginate-v2 */
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+};
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+        _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model('User', userSchema)
